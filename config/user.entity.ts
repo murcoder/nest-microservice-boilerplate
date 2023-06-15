f9c0b00b96baf47a6378cdc931904e6571b45373
murcoder <chris.murauer@yahoo.de>
2023-06-15 17:09:56 +0200

    added user

---
+++ config/user.entity.ts
@@ -0,0 +1,35 @@
+import {Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
+import * as bcrypt from 'bcrypt'
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    @DeleteDateColumn()
+    deleted_at: Date
+
+    async genSalt(): Promise<string> {
+        return bcrypt.genSalt(42);
+    }
+
+    async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.has(password, salt);
+    }
+
+    async validatePassword(password: string): Promise<boolean> {
+        return await bcrypt.compare(password, this.password);
+    }
+}
